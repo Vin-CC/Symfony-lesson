7476dfc7e0f1251b6ce70bbc253488b6bcd65a4d
Vin-CC <vincent.cance@gmail.com>
2022-10-13 09:57:47 +0200

    correct typo

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -1,11 +1,11 @@
 <?php
-namespace App\Form\Tyoe;
+namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\TestType;
-use Symfony\Component\Form\FormBuilder\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class TaskType extends AbstractType
 {
